e48788d5e675977328bc141f98f51ed7d7afbdb4
dchiapa <diegochiapa@gmail.com>
2020-03-17 20:59:30 -0300

    Página de inicio barra de busqueda

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
 	include 'html/header.html';
 ?>
-<main>
+<main id="ppal">
 	<h2>Propiedades en venta y en alquiler</h2>
 	<h3>El hogar que soñaste a tu alcance</h3>
-	<ul>
-		<li><button id="comprar" data-dato="comprar">Compra</button></li>
-		<li><button id="alquiler" data-dato="alquiler">Alquiler</button></li>
-		<li><button id="temporal" data-dato="temporal">Temporal</button></li>
-	</ul>
-	<select name="propTipo" id="propTipo">
-		<option value="">Seleccione una opción</option>
-		<!-- 17-03-20 PHP rellenar opciones -->
-	</select>
-	<input type="text" name="propUbicacion" placeholder="Ingrese un barrio">
-	<button>Buscar</button>
+	<div id="buscar">
+		<ul>
+			<button id="comprar" data-dato="comprar">Compra</button>
+			<button id="alquiler" data-dato="alquiler">Alquiler</button>
+			<button id="temporal" data-dato="temporal">Temporal</button>
+		</ul>
+		<div id="buscar2">
+			<select name="propTipo" id="propTipo">
+				<option value="">Seleccione una opción</option>
+				<!-- 17-03-20 PHP rellenar opciones -->
+			</select>
+			<input type="text" name="propUbicacion" placeholder="Ingrese un barrio">
+			<button>Buscar</button>
+		</div>
+	</div>
 </main>
 <?php
 	include 'html/footer.html';
