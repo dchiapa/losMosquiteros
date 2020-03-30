4fcc5b55edaa3279a2347fbbfb68aca254421481
ferfit <ferfit16@gmail.com>
2020-03-30 15:36:43 -0600

    cambios en verpro

--- funciones/propiedad.php
+++ funciones/propiedad.php
@@ -26,10 +26,30 @@ return $resultado;
 
 function buscarPropiedades()
 {
-$idEstado = $_GET['idEstado'];
+$idPropiedad = 1;
+/*$idEstado = $_GET['idEstado'];
 $idTipo = $_GET['idTipo'];
 $idBarrio = $_GET['idBarrio'];
-
+*/
 $link = conectar();
+$sql = "SELECT idPropiedad,
+                p.idEstado, valorEstado,
+                p.idTipo, valorTipo,
+                p.idBarrio, valorBarrio, 
+                ambientes, proTitulo,  proDireccion, proPrecio, proDescripcion,
+                proDormitorios, proBaños, proSupTotal, proSupCubierta, proSupSemi, proAntiguedad,
+                proCocheras, proPileta,proQuincho,proParrilla,proJardin,
+                proLuzEle,proAguaCorriente,proAguaPozo,proGasNatural,proGasEmbasado,proCloacas
+             
+                FROM propiedades p, estado e, tipo t, barrio b
+                WHERE p.idEstado = e.idEstado
+                AND   p.idTipo= t.idTipo
+                AND   p.idBarrio = b.idBarrio
+                AND   idPropiedad = ".$idPropiedad = 1;
+
+$resultado = mysqli_query($link, $sql)
+                        or die(mysqli_error($link));
+
+return mysqli_fetch_assoc($resultado);
 
 }
--- verPropiedad.php
+++ verPropiedad.php
@@ -6,7 +6,41 @@
     require 'funciones/propiedad.php';
 	$tipos = listarTipos();
 	$estados = listarEstados();
-    $barrios = listarBarrios();
-    $propiedades = listarPropiedades();
+    $barrios = listarBarrios(); 
+    $propiedades = buscarPropiedades();
 	include 'html/header2.html';
 ?>
+
+
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/fondo3.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/fondo1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+<div class="card">
+  <h5 class="card-header"><?= $propiedades['proTitulo']; ?></h5>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
