3db8385a9966563cf255535dcead20da297a7760
dchiapa <diegochiapa@gmail.com>
2020-03-31 20:39:29 -0300

    admin de barrios

--- admin.php
+++ admin.php
@@ -18,7 +18,7 @@
 			<h2 class="d-block text-center my-5">Panel de administración</h2>
 			<div class="row">
 				<div class="list-group col-6 mx-auto">
-					<a href="#" class="list-group-item list-group-item-action text-center">Panel de administración de Barrios</a>
+					<a href="adminBarrios.php" class="list-group-item list-group-item-action text-center">Panel de administración de Barrios</a>
 					<a href="adminEstados.php" class="list-group-item list-group-item-action text-center">Panel de administración de Estados</a>
 					<a href="adminTipos.php" class="list-group-item list-group-item-action text-center">Panel de administración de Tipos</a>
 					<a href="#" class="list-group-item list-group-item-action text-center">Panel de administración de Usuarios</a>
---
+++ adminBarrios.php
@@ -0,0 +1,57 @@
+<?php
+    require 'funciones/conexion.php';
+    require 'funciones/barrio.php';
+    $barrios = listarBarrios();
+	include 'html/header.html';
+?>
+<body>
+    <main id="adminEstados">
+        <header class="card-header border-0">
+            <img src="img/logo2.jpeg" alt="Logo de Los mosquiteros">
+            <h1 class="d-none">Los Mosquiteros</h1>
+            <nav>
+                <ul>
+        	 	    <li class="d-inline"><a href="index.php" class="btn btn-outline-light py-2 px-5">Inicio</a></li>
+        	 	    <li class="d-inline"><a href="" class="btn btn-outline-light py-2 px-5">Publicar</a></li>
+        	 	    <li class="d-inline"><a href="admin.php" class="btn btn-outline-light py-2 px-5">LogIn</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="col-6 mx-auto">
+            <h2 class="d-block text-center my-5">Panel de administración de barrios</h2>
+            <a href="admin.php" class="btn btn-outline-secondary m-2">Volver a principal</a>
+            <table class="table table-hover table-border table-striped">
+                <thead class="thead-dark">
+        	    	<tr>
+                        <th class=" text-center">Id</th>
+        	    		<th class=" text-center">Barrio</th>
+        	        	<th colspan="3">
+        	        		<a href="formAgregarEstado.php" class="btn btn-secondary">Agregar</a>
+        	    		</th>
+        	    	</tr>
+                </thead>
+                <tbody>
+<?php
+    while($barrio = mysqli_fetch_assoc($barrios)){
+?>
+                    <tr>
+                        <td class=" text-center"><?= $barrio['idBarrio'] ?></td>
+                        <td class=" text-center"><?= $barrio['valorBarrio'] ?></td>
+                        <td>
+	    		    		<a href="formModificarBarrio.php?idBarrio=<?= $barrio['idBarrio'] ?>" class="btn btn-outline-secondary">Modificar</a>
+                        </td>
+	    		    	<td>
+	    		    		<a href="formEliminarBarrio.php?idBarrio=<?= $barrio['idBarrio'] ?>" class="btn btn-outline-secondary">Eliminar</a>
+                        </td>
+                    </tr>
+<?php
+    }
+?>
+                </tbody>
+            </table>
+            <a href="admin.php" class="btn btn-outline-secondary m-2">Volver a principal</a>
+        </div>
+    </main>
+<?php
+	include 'html/footer.html';
+?>
