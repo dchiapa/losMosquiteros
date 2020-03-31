0c3b09a6d01291f4c47f1ed8d8e9aed6ef5d956c
dchiapa <diegochiapa@gmail.com>
2020-03-31 12:28:52 -0300

    adaptacion fer-1

--- admin.php
+++ admin.php
@@ -1,19 +1,31 @@
 <?php
 	include 'html/header.html';
 ?>
-<main id="admin">
+<body>
+	<main id="admin">
+    <header>
+        <img src="img/logo2.jpeg" alt="Logo de Los mosquiteros">
+        <h1>Los Mosquiteros</h1>
+        <nav>
+            <ul>
+                <li class="btn btn-outline-secondary"><a href="index.php" class="text-dark">Inicio</a></li>
+                <li class="btn btn-outline-secondary"><a href="" class="text-dark">Publicar</a></li>
+                <li class="btn btn-outline-secondary"><a href="admin.php" class="text-dark">LogIn</a></li>
+            </ul>
+        </nav>
+    </header>
 	<div class="col-6 mx-auto">
-	<h2 class="d-block text-center my-5">Panel de administración</h2>
-	<div class="row">
-		<div class="list-group col-6 mx-auto">
-			<a href="#" class="list-group-item list-group-item-action text-center">Panel de administración de Barrios</a>
-			<a href="adminEstados.php" class="list-group-item list-group-item-action text-center">Panel de administración de Estados</a>
-			<a href="adminTipos.php" class="list-group-item list-group-item-action text-center">Panel de administración de Tipos</a>
-			<a href="#" class="list-group-item list-group-item-action text-center">Panel de administración de Usuarios</a>
+		<h2 class="d-block text-center my-5">Panel de administración</h2>
+		<div class="row">
+			<div class="list-group col-6 mx-auto">
+				<a href="#" class="list-group-item list-group-item-action text-center">Panel de administración de Barrios</a>
+				<a href="adminEstados.php" class="list-group-item list-group-item-action text-center">Panel de administración de Estados</a>
+				<a href="adminTipos.php" class="list-group-item list-group-item-action text-center">Panel de administración de Tipos</a>
+				<a href="#" class="list-group-item list-group-item-action text-center">Panel de administración de Usuarios</a>
+			</div>
 		</div>
 	</div>
-</div>
-</main>
+	</main>
 <?php
 	include 'html/footer.html';
 ?>
--- index.php
+++ index.php
@@ -6,41 +6,53 @@
 	$estados = listarEstados();
 	include 'html/header.html';
 ?>
-<main id="ppal">
-	<h2>Propiedades en venta y en alquiler</h2>
-	<form action="" method="get" class="container">
-		<div class="row">
-			<div class="col-3"></div>
+<body>
+	<main id="ppal">
+		<header class="card-header border-0">
+			<img src="img/logo2.jpeg" alt="Logo de Los mosquiteros">
+			<h1 class="">Los Mosquiteros</h1>
+			<nav>
+			    <ul>
+			        <li class="btn btn-outline-secondary"><a href="index.php">Inicio</a></li>
+			        <li class="btn btn-outline-secondary"><a href="">Publicar</a></li>
+			        <li class="btn btn-outline-secondary"><a href="admin.php">LogIn</a></li>
+			    </ul>
+			</nav>
+		</header>
+		<h2>Propiedades en venta y en alquiler</h2>
+		<form action="" method="get" class="container">
+			<div class="row">
+				<div class="col-3"></div>
 <?php
    	while ($estado = mysqli_fetch_assoc($estados)) {            	
 ?>
-			<button type="button" value="<?= $estado['idEstado'] ?>" id="<?= $estado['idEstado'] ?>" class=" btnEstado btn btn-secondary col-2 m-2"><?= $estado['valorEstado'] ?></button>
+				<button type="button" value="<?= $estado['idEstado'] ?>" id="<?= $estado['idEstado'] ?>" class=" btnEstado btn btn-secondary col-2 m-2"><?= $estado['valorEstado'] ?></button>
 <?php 
 	}
 ?>
-		</div>
-		<div class="row">
-			<div class="col-1"></div>
-			<select name="propTipo" id="propTipo" class="border border-dark rounded">
-			<option value=""> Seleccione una opción</option>
+			</div>
+			<div class="row">
+				<div class="col-1"></div>
+				<select name="propTipo" id="propTipo" class="border border-dark rounded">
+					<option value=""> Seleccione una opción</option>
 <?php
    	while ($tipo = mysqli_fetch_assoc($tipos)) {            	
 ?>
-            	<option value="<?= $tipo['idTipo'] ?>"><?= $tipo['valorTipo'] ?></option>
+    				<option value="<?= $tipo['idTipo'] ?>"><?= $tipo['valorTipo'] ?></option>
 <?php 
 	}
 ?>
-			</select>
-			<input type="text" name="propUbicacion" placeholder="Ingrese un barrio" class="border border-dark rounded col-6 mx-2">
-			<input type="hidden" id="idEstado" name="idEstado" value="">
-			<button class="btn btn-info col-2">Buscar</button>
-			<div class="col-1"></div>
-		</div>
-		<div class="col-3"></div>
-	</form> 
-	<h3>El hogar que soñaste a tu alcance</h3>
-	<script src="js/estado.js"></script>
-</main>
+				</select>
+				<input type="text" name="propUbicacion" placeholder="Ingrese un barrio" class="border border-dark rounded col-6 mx-2">
+				<input type="hidden" id="idEstado" name="idEstado" value="">
+				<button class="btn btn-info col-2">Buscar</button>
+				<div class="col-1"></div>
+			</div>
+			<div class="col-3"></div>
+		</form> 
+		<h3>El hogar que soñaste a tu alcance</h3>
+		<script src="js/estado.js"></script>
+	</main>
 <?php
 	include 'html/footer.html';
 ?>
