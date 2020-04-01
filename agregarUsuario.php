e40a073e47c2d015096ccd27dc2525af49c311da
ferfit <ferfit16@gmail.com>
2020-04-01 14:50:37 -0300

    ok agregarUsuario

---
+++ agregarUsuario.php
@@ -0,0 +1,38 @@
+<?php
+	require 'funciones/conexion.php';
+	require 'funciones/usuarios.php';
+	include 'html/header.html';
+	$chequeo = agregarUsuario();
+?>
+<body>
+	<main class="agregar">
+		<header class="card-header border-0">
+			<img src="img/logo2.jpeg" alt="Logo de Los mosquiteros">
+			<nav>
+			    <ul>
+			        <li class="d-inline"><a href="index.php" class="btn btn-outline-light py-2 px-5">Inicio</a></li>
+			        <li class="d-inline"><a href="" class="btn btn-outline-light py-2 px-5">Publicar</a></li>
+			        <li class="d-inline"><a href="admin.php" class="btn btn-outline-light py-2 px-5">LogIn</a></li>
+			    </ul>
+			</nav>
+		</header>
+
+	    <div class="col-6 mx-auto text-center">
+            <h1 class="d-block text-center my-5" >Agregar usuario</h1>
+<?php 
+    $class = 'danger';
+    $mensaje = 'Nose pudo agregar el usuario';
+    if ($chequeo){ 
+    	$class = 'success';
+    	$mensaje = 'Usuario agregado correctamente.';
+    }
+?>
+			<div class="alert alert-<?= $class; ?>">
+				<?= $mensaje ?>
+			</div>
+			<a href="adminUsuarios.php" class="btn btn-outline-secondary m-2">Volver a usuarios</a>
+		</div>
+	</main>
+<?php
+	include 'html/footer.html';
+?>
---
+++ formAgregarUsuario.php
@@ -0,0 +1,40 @@
+<?php
+    require 'funciones/conexion.php';
+    require 'funciones/usuarios.php';
+    include 'html/header.html';
+    $barrios = listarUsuarios();
+?>
+<body>
+    <main class="formAgregar">
+        <header class="card-header border-0">
+            <img src="img/logo2.jpeg" alt="Logo de Los mosquiteros">
+            <h1 class="d-none">Los Mosquiteros</h1>
+            <nav>
+                <ul>
+        	 	    <li class="d-inline"><a href="index.php" class="btn btn-outline-light py-2 px-5">Inicio</a></li>
+        	 	    <li class="d-inline"><a href="" class="btn btn-outline-light py-2 px-5">Publicar</a></li>
+        	 	    <li class="d-inline"><a href="admin.php" class="btn btn-outline-light py-2 px-5">LogIn</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="col-6 mx-auto">
+            <h2 class="d-block text-center my-5">Agregar Usuario</h2>
+            <form action="agregarUsuario.php" method="POST">
+                <label>Nombre: </label>
+                <input type="text" name="usuNombre" class="form-control">
+                <label>Apellido: </label>
+                <input type="text" name="usuApellido" class="form-control">
+                <label>Email: </label>
+                <input type="text" name="usuEmail" class="form-control">
+                <label>Contraseña: </label>
+                <input type="text" name="usuPass" class="form-control">
+                <label>Estado: </label>
+                <input type="text" name="usuEstado" class="form-control">
+                <button class="btn btn-dark">Agregar</button>
+                <a href="admin.php" class="btn btn-outline-secondary m-2">Volver a principal</a>
+            </form>
+        </div>
+    </main>
+<?php
+	include 'html/footer.html';
+?>
