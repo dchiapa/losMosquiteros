df5ac5b57e79fcd02fadb072b5aff62a4f34af9b
dchiapa <diegochiapa@gmail.com>
2020-04-02 12:40:26 -0300

    index boton buscar

--- index.php
+++ index.php
@@ -20,14 +20,14 @@
 			</nav>
 		</header>
 		<h2 class="text-light">Propiedades en venta y en alquiler</h2>
-		<form action="" method="get" class="container">
+		<form action="listadoDePropiedades.php" method="get" class="container">
 			<div class="row">
 				<div class="col-3"></div>
 <?php
-   	while ($estado = mysqli_fetch_assoc($estados)) {            	
+   	while ($estado = mysqli_fetch_assoc($estados)) {
 ?>
 				<button type="button" value="<?= $estado['idEstado'] ?>" id="<?= $estado['idEstado'] ?>" class=" btnEstado btn btn-secondary col-2 m-2 py-2 px-5"><?= $estado['valorEstado'] ?></button>
-<?php 
+<?php
 	}
 ?>
 			</div>
@@ -36,10 +36,10 @@
 				<select name="propTipo" id="propTipo" class="border border-dark rounded px-2">
 					<option value=""> Seleccione una opción</option>
 <?php
-   	while ($tipo = mysqli_fetch_assoc($tipos)) {            	
+   	while ($tipo = mysqli_fetch_assoc($tipos)) {
 ?>
     				<option value="<?= $tipo['idTipo'] ?>"><?= $tipo['valorTipo'] ?></option>
-<?php 
+<?php
 	}
 ?>
 				</select>
@@ -49,7 +49,7 @@
 				<div class="col-1"></div>
 			</div>
 			<div class="col-3"></div>
-		</form> 
+		</form>
 		<h3 class="text-primary">El hogar que soñaste a tu alcance</h3>
 		<script src="js/estado.js"></script>
 	</main>
