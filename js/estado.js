a9e37539b5359535b997438ba0f70221c11bdfca
dchiapa <diegochiapa@gmail.com>
2020-04-08 19:18:22 -0300

    estados, seleccion de estados puede dehacer

--- js/estado.js
+++ js/estado.js
@@ -1,13 +1,12 @@
 // Lee un elemento a traves del id
 // como los id son unicos trae el unico elemento
 let input = document.getElementById('idEstado');
-// Lee un elemento a traves de la clase
-// como las clases se pueden repetir trae todas en forma de array
+// Lee un elemento a traves de la class
+// como las class se pueden repetir trae todas en forma de array
 let botones = document.getElementsByClassName('btnEstado');
 // Declaración de una variable
 let boton;
 // Recorre el array
-
 for (let i = 0; i < botones.length; i++) {
     // escucha el evento click en los botones
     // Si se hace click en algun boton, ejecuta la funcion boton apretado
@@ -16,22 +15,26 @@ for (let i = 0; i < botones.length; i++) {
 }
 // e es una variable que tiene todos los datos del evento escuchado
 function botonApretado(e) {
-    botones = document.getElementsByClassName('activo');
-    // Recorre el array 
-    for (let i = 0; i < botones.length; i++) {
-        // Reemplaza la clase btn-success por btn-scondary
-        boton = botones[i].classList.replace('btn-success', 'btn-secondary');
-        // Elimina la clase activo
-        boton = botones[i].classList.remove('activo');
-    }
     // Selecciona el boton en el cual se hizo click
     boton = e.target;
-    // Agraga la clase activo
-    boton.classList.add('activo');
-    // Reemplaza la clase btn-scondary por btn-success
-    boton.classList.replace('btn-secondary', 'btn-success');
-    // En el input de idEstado carga el valor que tiene el boton
-    input.value = boton.value;
+    // Si el boton apretado contiene la class activo
+    if (boton.classList.contains('activo')) {
+        // Reemplaza la class btn-success por btn-scondary
+        boton.classList.replace('btn-success', 'btn-secondary');
+        // Elimina la class activo
+        boton.classList.remove('activo');
+        // pone en el input el valor 0
+        input.value = 0;
+    } else {
+        for (let i = 0; i < botones.length; i++) {
+            botones[i].classList.replace('btn-success', 'btn-secondary');
+            botones[i].classList.remove('activo');
+        }
+        // Agrega la class activo
+        boton.classList.add('activo');
+        boton.classList.replace('btn-secondary', 'btn-success' );
+        input.value = boton.value;
+    }
     // Con console.log(); podes ver los valores en la consola del browser
     // Por EJ console.log(input); te va a mostrar el input hidden
 }
